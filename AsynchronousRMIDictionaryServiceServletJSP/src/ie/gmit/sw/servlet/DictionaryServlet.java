0adcc8a3aba8b1ebe64b57ccfcefadd0a458de24
alexpt2000gmit <g00317835@gmit.ie>
2017-11-21 17:10:40 +0000

    The Servlet and JSP do resquest on Server and print on console defintions of dictionary

--- AsynchronousRMIDictionaryServiceServletJSP/src/ie/gmit/sw/servlet/DictionaryServlet.java
+++ AsynchronousRMIDictionaryServiceServletJSP/src/ie/gmit/sw/servlet/DictionaryServlet.java
@@ -1,54 +1,84 @@
 package ie.gmit.sw.servlet;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import ie.gmit.sw.ServiceSetup.DictionaryServiceInterface;
+
 /**
  * Servlet implementation class DictionaryServlet
  */
 @WebServlet("/ola")
 public class DictionaryServlet extends HttpServlet {
+
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DictionaryServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	private static DictionaryServiceInterface look_up;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public DictionaryServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+
+		System.out.println("Stating Server.....");
+
+		try {
+			look_up = (DictionaryServiceInterface) Naming.lookup("//localhost/RMIdictionary");
+		} catch (MalformedURLException | RemoteException | NotBoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		String keyWord = request.getParameter("keyWord");
 
-		System.out.println(keyWord);
+		System.out.println(keyWord.toUpperCase());
 
-		if (keyWord.equals("get")) {
+		ArrayList<String> responseDefinition = look_up.findDictionary(keyWord.toUpperCase());
 
-			response.sendRedirect("response.jsp");
-
-		} else {
-
-			response.sendRedirect("notfound.jsp");
-
+		for (String string : responseDefinition) {
+			System.out.print(string);
 		}
-		
+
+		response.sendRedirect("index.jsp");
+
 	}
 
 }
