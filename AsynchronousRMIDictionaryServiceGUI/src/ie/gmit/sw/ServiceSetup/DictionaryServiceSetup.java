6da629bc86afabef602af5d33b2114c384637267
alexpt2000gmit <g00317835@gmit.ie>
2017-11-21 11:03:40 +0000

    Dictionary works, pass by console RMI

--- AsynchronousRMIDictionaryServiceGUI/src/ie/gmit/sw/ServiceSetup/DictionaryServiceSetup.java
+++ AsynchronousRMIDictionaryServiceGUI/src/ie/gmit/sw/ServiceSetup/DictionaryServiceSetup.java
@@ -15,54 +15,7 @@ import java.util.function.Predicate;
 
 public class DictionaryServiceSetup {
 
-	protected DictionaryServiceSetup() throws RemoteException {
-
-	}
-
-	private static Dictionary initializeList() throws IOException {
-
-		// Variables
-		String inputLine = null;
-		String quotes;
-		String oldWordKey = "";
-
-		// Reference name for the file dictionary
-		BufferedReader reader = new BufferedReader(new FileReader(new File("dictionary.csv")));
-
-		// Instantiate class WordDetail
-		Dictionary wordDetail = new Dictionary();
-
-		// load the file dictionary into a HashMap an Arralist
-		while ((inputLine = reader.readLine()) != null) {
-			String[] wordsKey = inputLine.split(",", 2);
-
-			// Ignore empty lines.
-			if (inputLine.equals("")) {
-				continue;
-			}
-
-			// Take first character into variable quotes
-			quotes = wordsKey[0].substring(0, 1);
-
-			// replace quotes by blank space
-			wordsKey[0] = wordsKey[0].replace("\"", "");
-
-			// if first character in this line is quotes, this line have key and
-			// definition
-			if ("\"".equals(quotes)) {
-				oldWordKey = wordsKey[0];
-				wordDetail.addDicWord(oldWordKey.toUpperCase(), inputLine);
-
-				// System.out.println(inputLine);
-			}
-			// if not, is just definition
-			else {
-				wordDetail.addDicWord(oldWordKey.toUpperCase(), inputLine);
-			}
-		}
-
-		return wordDetail;
-	}
+	static LoadDictionary loadDictionary;
 
 	public static void main(String[] args) {
 		try {
@@ -70,7 +23,8 @@ public class DictionaryServiceSetup {
 			// Start the RMI regstry on port 1099
 			LocateRegistry.createRegistry(1099);
 
-			Naming.rebind("//localhost/RMIdictionary", new DictionaryServiceImpl(initializeList()));
+			Naming.rebind("//localhost/RMIdictionary",
+					new DictionaryServiceImpl(loadDictionary.initializeDictionary()));
 			System.err.println("Server ready");
 		} catch (Exception e) {
 			System.err.println("Server exception: " + e.getMessage());
